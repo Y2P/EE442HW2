7f830a81f5e113203f21d7eec6154597aea6d94b
Y2P <yigit.pilavci01@gmail.com>
2017-04-15 21:55:50 +0300

    HW2

--- client.c
+++ client.c
@@ -20,14 +20,21 @@
 
 pthread_mutex_t writelock1;
 pthread_mutex_t writelock2;
-int ThreadNum;
-int ret;
-sem_t *STC_sem_full;
-sem_t *STC_sem_empty;
-sem_t *CTS_sem_full; 
-sem_t *CTS_sem_empty; 
 
+
+// Necessary integer values
+int ThreadNum;
 int fd;
+
+// Semaphores are created
+// The wait conditions are condisered for chosing their missions
+sem_t *STC_sem_full; // Wait semaphore when Server to Client (STC) queue is full
+sem_t *STC_sem_empty; // Wait semaphore when Server to Client (STC) queue is empty
+sem_t *CTS_sem_full; // Wait semaphore when Client to Server (CTS) queue is full
+sem_t *CTS_sem_empty;  // Wait semaphore when Client to Server (CTS) queue is empty
+
+
+// Message Formats are defined here
 struct client_message 
 {
 	pthread_t client_id;
@@ -39,6 +46,7 @@ struct server_message
 	pid_t server_id;
 	int answer;
 };
+// Shared Memory Prototype
 struct SharedMem
 {
 
@@ -51,51 +59,76 @@ struct SharedMem
 	struct client_message CTS_Q[MAXSIZE];
 };
 
-
+// Sender thread
 void* sender(void* SM)
 {
 
+	// Read the necessary semaphores
 	CTS_sem_full = sem_open(SEMCTSFULL,0);
 	CTS_sem_empty = sem_open(SEMCTSEMPTY,0);
+
+	// If CTS full, block the thread
 	sem_wait(CTS_sem_full);
-	sem_post(CTS_sem_empty);
+
+	// Initialize the temporary variables
 	struct client_message m;
 	struct SharedMem* s;
 
+	// Lock the write to Shared Memory
 	
 	pthread_mutex_lock(&writelock1);
 
 		s = ((struct SharedMem*)SM);
 		pthread_t i = pthread_self();
 		m.client_id = i;
+
+		// Keep question in 3 digit
 		m.question = rand()%1000;
+		if(m.question < 100)
+			m.question  = m.question +100;
+		// Push new message
+		printf("id : %d m: %d \n",i,m.question );
 		s->CTS_Q[s->CTSFront] = m;
+		// Circular queue
 		(++s->CTSFront);
 		s->CTSFront = (s->CTSFront)%(s-> NumOfQueueElem);
-
+		sem_post(CTS_sem_empty);
+	// Unlock the operation 
 	pthread_mutex_unlock(&writelock1);
 }
+
+// Reciever method is defined here
 void* reciever(void* SM)
 {	
+	
+	// Initialize the random number generator
+	srand(time(NULL) ^ pthread_self()<<16);
+	// Read the necessary semaphores
+
 	STC_sem_empty = sem_open(SEMSTCEMPTY,0);
 	STC_sem_full = sem_open(SEMSTCFULL,0);
 
+	// Wait if STC is empty
 	sem_wait(STC_sem_empty);
-	sem_post(STC_sem_full);
+	// Define temporary variables
 	struct SharedMem* s;
-	
+	// Lock the pop operation on Shared Memory
 	pthread_mutex_lock(&writelock2);
-		pthread_t i = pthread_self();
+
 		s = ((struct SharedMem*)SM);
-		printf("From server process %d to client thread %d - Result: %d\n",(int)s->STC_Q[s->STCRear].server_id,(int)i,(int)s->STC_Q[s->STCRear].answer);
+		// Print the result from server
+		printf("From server process %d to client thread %d - Result: %d\n",(int)s->STC_Q[s->STCRear].server_id,(int)s->STC_Q[s->STCRear].client_id ,(int)s->STC_Q[s->STCRear].answer);
+		// Pop the message in the queue
 		s->STC_Q[s->STCRear].client_id =0;
 		s->STC_Q[s->STCRear].server_id =0;
 		s->STC_Q[s->STCRear].answer =0;
+		// Circular queue indexes are adjusted
 		s->STCRear++;
 		s->STCRear = s->STCRear % s->NumOfQueueElem;
-
+		sem_post(STC_sem_full);
 	pthread_mutex_unlock(&writelock2);
 }
+// Necessary variables are defined here
 struct SharedMem* SM;
 void *takesize;
 pthread_t* tid_sender;
@@ -104,6 +137,8 @@ void* b;
 int i= 0;
 int main(int argc, char const *argv[])
 {
+
+	// Client main process 
 	char buffer[30];
 	time_t raw;
 	struct tm * timeinfo;
@@ -111,13 +146,15 @@ int main(int argc, char const *argv[])
 	timeinfo = localtime(&raw);
 	strftime(buffer,30,"%a %b %d %X %Y",timeinfo );
 	printf("Client program started at %s \n", buffer);
+
+	// Read the number of threads
 	if(argc > 2)
 	{
 		ThreadNum = atoi(argv[2]);
 	}
 	else
 		ThreadNum = 10;
-
+// Open the shared memory
 	fd = shm_open("/shared",O_RDWR,0600);
 // Shared Memory is read
 	takesize = mmap((void *)0x20000000,sizeof(int),PROT_READ|PROT_WRITE,MAP_FIXED|MAP_SHARED,fd,0); 
@@ -125,21 +162,26 @@ int main(int argc, char const *argv[])
 	int Qsize=*(int*)takesize;
 	int RegionSize = 3*sizeof(int) + Qsize*sizeof(struct server_message) + 2*sizeof(int) +Qsize*sizeof(struct client_message);
 
+	// Open the shared file
 	fd = shm_open("/shared",O_RDWR,0600);
+	// Read the whole file
 	b = (void*)mmap((void *)0x20000000,RegionSize,PROT_READ|PROT_WRITE,MAP_FIXED|MAP_SHARED,fd,0);
-
+	// Type cast raw data to prototype
 	SM = ((struct SharedMem*)b );
 
-	
+	// Allocate the sender and reciever threads
 	tid_sender= (pthread_t*)malloc(ThreadNum*sizeof(pthread_t));
 	tid_receiver= (pthread_t*)malloc(ThreadNum*sizeof(pthread_t));
+
 	for ( i = 0;i<ThreadNum;i++)
 	{
+		// Create and spawn the threads
 		pthread_create(&(tid_sender[i]),NULL,&sender, (void*) SM);
 		pthread_create(&(tid_receiver[i]),NULL,&reciever,(void*)SM);
 	}
 
 // Release the allocated memories
+	// Wait number of thread seconds by blocking the main process.
 	sleep(ThreadNum);
 	munmap(SM,RegionSize);
 
--- server.c
+++ server.c
@@ -19,10 +19,11 @@
 #define SEMSTCEMPTY	"/semstc_empty"
 #define SEMCTSEMPTY	"/semcts_empty"
 
-
+// Memory write operation locks are initialized
 pthread_mutex_t writelock;
 pthread_mutex_t writelock2;
 
+// Message Formats are initialized
 struct server_message 
 {
 	pthread_t client_id;
@@ -34,6 +35,8 @@ struct client_message
 	pthread_t client_id;
 	int question;
 };
+
+// Shared Memory is prototyped here. It is used as a template further casting operations
 struct SharedMem
 {
 
@@ -45,32 +48,34 @@ struct SharedMem
 	int CTSRear ;
 	struct client_message CTS_Q[MAXSIZE];
 };
-
+// Signal ISR initialized
 void INThandler(int);
+
+// Necessary integer values
 int SharedMemSize;
 int ret;
-int fd;
 
-sem_t *STC_sem_full;
-sem_t *STC_sem_empty;
-sem_t *CTS_sem_full; 
-sem_t *CTS_sem_empty; 
+// Semaphores are created
+// The wait conditions are condisered for chosing their missions
+sem_t *STC_sem_full; // Wait semaphore when Server to Client (STC) queue is full
+sem_t *STC_sem_empty; // Wait semaphore when Server to Client (STC) queue is empty
+sem_t *CTS_sem_full; // Wait semaphore when Client to Server (CTS) queue is full
+sem_t *CTS_sem_empty;  // Wait semaphore when Client to Server (CTS) queue is empty
 
-pid_t  pid;
+
+
+// Shared memory structure pointer is defined 
 struct SharedMem *SM;
+// Status for waitpid
 int status=0;
+int fd;
+// Indicator for parent process.
 int x = 0;
 
-
+// ISR for catching CTRL-C
 void  INThandler(int sig)
 {
-	   //  free(SM->STC_Q);
-   //free(SM->CTS_Q);
-   // Unmap the object
-   
-   //printf("Do you have to go .... \n");
-   //printf("See ya!!! \n");
-
+   // Unlink and unmap operations
    munmap(SM,sizeof(struct SharedMem) );
    sem_unlink(SEMSTCFULL);
    sem_unlink(SEMCTSFULL);
@@ -80,17 +85,20 @@ void  INThandler(int sig)
    close(fd);
    // Remove the shared memory object
    shm_unlink("/shared");
-  	if(x == 0)
-	{// Print the modification time in proper format
-	char buffer[7];
-	time_t raw;
-	struct tm * timeinfo;
-	time(&raw);
-	timeinfo = localtime(&raw);
-	strftime(buffer,30,"%a %b %d %X %Y",timeinfo );
-	printf("Server program closed at %s \n", buffer);
+  	if(x == 1)
+	{
+		x++;
+		printf("Do you have to go .... \n");
+		printf("See ya!!! \n");
+		// Print the modification time in proper format
+		char buffer[30];
+		time_t raw;
+		struct tm * timeinfo;
+		time(&raw);
+		timeinfo = localtime(&raw);
+		strftime(buffer,30,"%a %b %d %X %Y",timeinfo );
+		printf("Server program closed at %s \n", buffer);
 	}
-	x++;
 
    exit(0);
 
@@ -98,7 +106,7 @@ void  INThandler(int sig)
 
 int main(int argc, char const *argv[])
 {
-
+	srand(time(NULL) ^ (getpid()<<16));
 
 	// Print the modification time in proper format
 	char buffer[30];
@@ -109,6 +117,7 @@ int main(int argc, char const *argv[])
 	strftime(buffer,30,"%a %b %d %X %Y",timeinfo );
 	printf("Server program started at %s \n", buffer);
 
+	// Get memory size
 	if(argc > 2)
 	{
 		SharedMemSize = atoi(argv[2]);
@@ -116,8 +125,19 @@ int main(int argc, char const *argv[])
 	else
 		SharedMemSize = 10;
 
+	// Calculate the necessary memory size (in byte)
 	int RegionSize = sizeof(int)*3  + SharedMemSize*sizeof(struct server_message) + sizeof(int)*2 +SharedMemSize*sizeof(struct client_message);
-// Initialize the queue semaphores
+	/*
+	Size
+	Front Index
+	Rear Index
+	Queue
+	Front Index
+	Rear Index
+	Queue
+	 */
+
+	// Initialize the queue semaphores
 	STC_sem_full = sem_open(SEMSTCFULL,O_CREAT,0644,3);
 	STC_sem_empty = sem_open(SEMSTCEMPTY,O_CREAT,0644,3);
 
@@ -129,20 +149,25 @@ int main(int argc, char const *argv[])
 	sem_init(&(*CTS_sem_empty),1,0);
 	sem_init(&(*STC_sem_empty),1,0);
 
+	// Open a shared file
 	fd = shm_open("/shared", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
 	if(fd == -1)
 		printf("The memory cannot created!!!\n");
 
+	// Cut the file by the prototyped structure
 	if(ftruncate(fd,sizeof(struct SharedMem )) == -1)
 		printf("Truncation is failed!!!\n");
 
+	// Map the shared file and output a pointer for this file
+	// Type casting is also applied here
 	SM = (struct SharedMem*)mmap((void *)0x20000000,RegionSize,PROT_READ|PROT_WRITE,
 				MAP_FIXED|MAP_SHARED,fd,0);
+
 
 	if (SM == MAP_FAILED)
 		printf("Mapping is failed!!!\n");
 	
-	
+	// Initialize the memory	
 	SM->NumOfQueueElem = SharedMemSize;
 	
 	SM->STCFront = 0;
@@ -150,36 +175,44 @@ int main(int argc, char const *argv[])
 	SM->CTSFront = 0;
 	SM->CTSRear = 0;
 
-
+	// Set an interrupt for CTRL-C
    	printf("CTRL-C to quit\n");
 	signal(SIGINT, INThandler);
 	
+	// Server is executed here
 	while(1)
 	{
-		if(	sem_wait(CTS_sem_empty) == 0)
-		{
+			// Parent indicator is increased
+			x++;
+			// Fork a child process
 			if(fork() != 0)
 				waitpid(-1,&status,0);
 			else
 			{
+			// If it is child 
+			// Check CTS is empty
+			sem_wait(CTS_sem_empty);
+			// Continues if it is not
+			// For alteration on shared memory
+			// Lock the process
 			pthread_mutex_lock(&writelock);
-
-				pthread_t i = pthread_self();
-
+				// Get the client id and question, and pop them
 				int passclientid = SM->CTS_Q[SM->CTSRear].client_id;
 				SM->CTS_Q[SM->CTSRear].client_id = 0;
 				int passquestion = SM->CTS_Q[SM->CTSRear].question;
 				SM->CTS_Q[SM->CTSRear].question = 0;
-
+				// Increment on Rear index
 				SM->CTSRear++;
+				// Circular queue
 				SM->CTSRear = SM->CTSRear % SM->NumOfQueueElem;
+
+				// Decrement in CTS_full semaphore for indicating a place is free in queue
 				sem_post(CTS_sem_full);
 			pthread_mutex_unlock(&writelock);
 			usleep(1000*(rand()%1000));
-
+			
+			sem_wait(STC_sem_full);
 			pthread_mutex_lock(&writelock2);
-				sem_wait(STC_sem_full);
-
 				SM->STC_Q[SM->STCFront].client_id = passclientid;
 				SM->STC_Q[SM->STCFront].server_id =getpid();
 				SM->STC_Q[SM->STCFront].answer = 2*passquestion;
@@ -191,7 +224,7 @@ int main(int argc, char const *argv[])
 			}
 		}
 
-	}
+	
 	
 
    return 0;
